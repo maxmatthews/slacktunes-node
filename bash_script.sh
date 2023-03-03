f7e611c0d25f05c4cc128977b2f08e6ecd30a0f2
Max Matthews <max@mzmtech.com>
2023-03-03 10:18:53 -0500

    Update bash script to use nvm's version of node Update request to fetch Switch from yarn to npm Fix indentation

--- bash_script.sh
+++ bash_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source ~/.zprofile
 cd /Users/maxmatthews/Developer/slacktunes-node
-/Users/maxmatthews/.nvm/versions/node/v12.18.1/bin/node index.js > /dev/null 2>&1
+node index.js > /dev/null 2>&1
